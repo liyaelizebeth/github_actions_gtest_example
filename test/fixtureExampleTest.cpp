81125e2b62b4c5b9a6cee28046f63ea97e8cc827
lji1cob <jose.liyaelizebeth@in.bosch.com>
2023-07-27 11:24:08 +0530

    Fixture

--- test/fixtureExampleTest.cpp
+++ test/fixtureExampleTest.cpp
@@ -1,36 +1,81 @@
-#include gtest/gtest.h
-class A{
-    B* ptr;
-    public:
-    A(B* addressOfObject): ptr{addressOfObject}{
+#include "gtest/gtest.h"
 
-    }
-    bool operation(){
-        return true;
-    }
-};
-class B{
-    C* ptr;
-    public:
-    B(C* addressOfObject): ptr{addressOfObject}{
-        
-    }
-};
 class C{};
 
-TEST(TestFixtureTestSuite,OperationTest){
+class B{
+
+    C* ptr;
+
+    public:
+
+    B(C* addressOfObject): ptr{addressOfObject}{
+
+
+
+
+    }
+
+};
+
+
+
+
+class A{
+
+    B* ptr;
+
+    public:
+
+    A(B* addressOfObject): ptr{addressOfObject}{
+
+
+
+
+    }
+
+    bool  operation(){
+
+        return true;
+
+    }
+
+};
+
+
+
+
+TEST(TestFixtureTestSuite,OperationTrueTest){
+
+    //Arrange
+
     C cObj;
+
     B bObj(&cObj);
+
     A obj(&bObj);
+
     //Act and Assert
+
     ASSERT_TRUE(obj.operation());
+
 }
 
-TEST(TestFixtureTestSuite,OperationTest){
+TEST(TestFixtureTestSuite,OperationFalseTest){
+
+    //Arrange
+
     C cObj;
-    B bObj(&cObj);
-    A obj(&bObj);
-    //Act and Assert
-    ASSERT_FALSE(obj.operation());
-}
 
+    B bObj(&cObj);
+
+    A obj(&bObj);
+
+    //Act and Assert
+
+    ASSERT_FALSE(obj.operation());
+
+
+
+
+
+}
